e03a6dbeec423004ba8fed1b86d16a6901739483
Ivan Notaros <nothke@gmail.com>
2021-10-03 03:09:52 +0200

    Water shader!

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Mathf;
 
 public class Water : MonoBehaviour
 {
     //public static Water e;
     //void Awake() { e = this; }
 
-    public static float Height => 0;
+    //public static float Height => 0;
     public static float Density => 100; // How much force to multiply to air
 
     public static bool IsUnderwater(Vector3 position)
     {
-        return position.y < Height;
+        return position.y < GetHeight(position);
     }
 
-    public static float GetHeight(Vector3 position)
+    public static float GetHeight(Vector3 pos)
     {
-        return Height;
+        float time = Time.time;
+
+        return Sin(time + pos.x * 1.1f) * 0.4f;
     }
 }
